4121228080ace5537b5e2846cd5284620e8a187f
Cat <61066141+Vimer1099@users.noreply.github.com>
2022-12-11 10:47:56 +0000

    1

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hi teacher")
+num1 = num2 = sign = ''
+while num1 == '' or num2 == '' or sign == '':
+	try:
+		num1 = int(input('Number1: '))
+		sign = input('Sign: ')
+		num2 = int(input('Number2: '))
+		if sign == '/' and num2 == 0:
+			raise ZeroDivisionError
+	except ValueError:
+		print('Wrong value')
+	except ZeroDivisionError:
+		sign = ''
+		print('Go to school')
+	except:
+		print('Something go wrong')
+	try:
+		if sign == '+':
+			print(f"{num1} + {num2} = {num1+num2}")
+		elif sign == '-':
+			print(f"{num1} - {num2} = {num1-num2}")
+		elif sign == '/':
+			print(f"{num1} / {num2} = {num1/num2}")
+	except TypeError:
+		print('Wrong types')
